92afb8ff3e499fb4d1ea9e3fd4c9ac1f1de76789
wangwenfeng01 <wangwenfeng01@baidu.com>
2018-07-30 15:36:10 +0800

    build test

--- build_ok.sh
+++ build_ok.sh
@@ -3,4 +3,4 @@ dependsdir = /home/zcash/depends/x86_64-unknown-linux-gnu
 
 
 
-cmake . -DBOOST_INCLUDE_DIR="$(dependsdir)"  -DBOOST_LIBRARY_DIR="$(dependsdir)"
+cmake . -DBOOST_INCLUDEDIR="$(dependsdir)"  -DBOOST_LIBRARYDIR="$(dependsdir)"
